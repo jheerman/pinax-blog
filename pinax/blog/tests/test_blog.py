d9433ff2a0b41574b78232051a85858564e96597
AronBodzas <aron.bodzas42@gmail.com>
2023-07-04 11:58:57 +0200

    creating tests for updating post

--- pinax/blog/tests/test_blog.py
+++ pinax/blog/tests/test_blog.py
@@ -210,4 +210,53 @@ class TestViews(TestBlog):
             self.assertEqual(response.context_data["post_list"].count(), 1)
             self.assertIn(self.orange2_post, response.context_data["post_list"])
 
+    def test_manage_update_post_get_author(self):
+        url = reverse("pinax_blog:manage_post_update", kwargs={"post_pk": "1"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+        with self.login(self.user):
+            url = reverse("pinax_blog:manage_post_update", kwargs={"post_pk": "1"})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed("pinax/blog/manage_post_update")
+
+    def test_manage_update_post_get_not_author(self):
+        self.user = self.make_user("jani")
+        self.user.save()
+
+        with self.login(self.user):
+            url = reverse("pinax_blog:manage_post_update", kwargs={"post_pk": "1"})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 404)
+
+        self.user.is_staff = True
+        self.user.save()
+
+        with self.login(self.user):
+            url = reverse("pinax_blog:manage_post_update", kwargs={"post_pk": "1"})
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+            self.assertTemplateUsed("pinax/blog/manage_post_update")
+
+    # def test_manage_update_post_post_author(self):
+    #     post_title = "Orange You Great"
+    #     post_data = Post.objects.create(
+    #         blog=self.blog,
+    #         section=self.oranges,
+    #         title=post_title,
+    #         slug=self.orange_slug,
+    #         author=self.user,
+    #         markup=self.markup,
+    #         state=Post.STATE_CHOICES[-1][0],
+    #     )
+    #     with self.login(self.user):
+    #         print(self.user)
+    #         self.post("pinax_blog:manage_post_update", data=post_data, kwargs={"post_pk": "1"})
+    #         self.assertRedirects(
+    #             self.last_response, reverse("pinax_blog:manage_post_list")
+    #         )
+    #         self.assertTrue(Post.objects.get(title=post_title))
+
+
 
