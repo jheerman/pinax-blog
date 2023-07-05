33017e336f6e9db4e2808d49b036d02800a2e6df
AronBodzas <aron.bodzas42@gmail.com>
2023-07-05 11:02:27 +0200

    refactoring tests for listing manageable blog posts

--- pinax/blog/tests/test_blog.py
+++ pinax/blog/tests/test_blog.py
@@ -174,10 +174,12 @@ class TestViews(TestBlog):
             )
             self.assertTrue(Post.objects.get(title=post_title))
 
-    def test_user_manage_post_list(self):
+    def test_user_manage_post_list_not_authenticated(self):
         url = reverse("pinax_blog:manage_post_list")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_user_manage_post_list_own_posts(self):
 
         with self.login(self.user):
             url = reverse("pinax_blog:manage_post_list")
@@ -186,24 +188,24 @@ class TestViews(TestBlog):
             self.assertIn(self.orange_post, response.context_data["post_list"])
             self.assertIn(self.apple_post, response.context_data["post_list"])
 
+    def test_user_manage_post_list_not_own_posts(self):
+
         self.user = self.make_user("jani")
         self.user.save()
-        self.orange2_title = "Orange You Wonderful2"
-        self.orange2_slug = slugify(self.orange_title)
-        self.orange2_post = Post.objects.create(
-            blog=self.blog,
-            section=self.oranges,
-            title=self.orange2_title,
-            slug=self.orange2_slug,
-            author=self.user,
-            markup=self.markup,
-            state=Post.STATE_CHOICES[-1][0],
-        )
+
         with self.login(self.user):
             url = reverse("pinax_blog:manage_post_list")
             response = self.client.get(url)
-            self.assertEqual(response.context_data["post_list"].count(), 1)
-            self.assertIn(self.orange2_post, response.context_data["post_list"])
+            self.assertEqual(response.context_data["post_list"].count(), 0)
+
+        self.user.is_staff = True
+        self.user.save()
+        with self.login(self.user):
+            url = reverse("pinax_blog:manage_post_list")
+            response = self.client.get(url)
+            self.assertEqual(response.context_data["post_list"].count(), 2)
+            self.assertIn(self.orange_post, response.context_data["post_list"])
+            self.assertIn(self.apple_post, response.context_data["post_list"])
 
 
     def test_manage_update_post_not_authenticated(self):
