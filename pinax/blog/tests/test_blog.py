82d8a83ddb7bebdf59b2e17ce285f242f113ab13
AronBodzas <aron.bodzas42@gmail.com>
2023-07-05 13:07:17 +0200

    refactoring duplicated code in tests

--- pinax/blog/tests/test_blog.py
+++ pinax/blog/tests/test_blog.py
@@ -137,7 +137,31 @@ class TestContextProcessors(TestBlog):
         self.assertEqual(result, {"scoper_lookup": ""})
 
 
-class TestViews(TestBlog):
+class SuccessfulDeletion:
+    def successful_deletion_for_test_blog(self):
+        url = reverse("pinax_blog:manage_post_delete", kwargs={"post_pk": "1"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.post(url)
+        self.assertRedirects(
+            self.last_response, reverse("pinax_blog:manage_post_list")
+        )
+        url = reverse("pinax_blog:manage_post_list")
+        response = self.client.get(url)
+        self.assertEqual(response.context_data["post_list"].count(), 1)
+        self.assertIn(self.apple_post, response.context_data["post_list"])
+
+
+class SuccessfulPostListing:
+    def successful_post_listing_for_test_blog(self):
+        url = reverse("pinax_blog:manage_post_list")
+        response = self.client.get(url)
+        self.assertEqual(response.context_data["post_list"].count(), 2)
+        self.assertIn(self.orange_post, response.context_data["post_list"])
+        self.assertIn(self.apple_post, response.context_data["post_list"])
+
+
+class TestViews(TestBlog, SuccessfulDeletion, SuccessfulPostListing):
     def test_manage_post_create_get(self):
         """
         Ensure template with external URL references renders properly
@@ -182,11 +206,7 @@ class TestViews(TestBlog):
     def test_user_manage_post_list_own_posts(self):
 
         with self.login(self.user):
-            url = reverse("pinax_blog:manage_post_list")
-            response = self.client.get(url)
-            self.assertEqual(response.context_data["post_list"].count(), 2)
-            self.assertIn(self.orange_post, response.context_data["post_list"])
-            self.assertIn(self.apple_post, response.context_data["post_list"])
+            self.successful_post_listing_for_test_blog()
 
     def test_user_manage_post_list_not_own_posts(self):
 
@@ -201,12 +221,7 @@ class TestViews(TestBlog):
         self.user.is_staff = True
         self.user.save()
         with self.login(self.user):
-            url = reverse("pinax_blog:manage_post_list")
-            response = self.client.get(url)
-            self.assertEqual(response.context_data["post_list"].count(), 2)
-            self.assertIn(self.orange_post, response.context_data["post_list"])
-            self.assertIn(self.apple_post, response.context_data["post_list"])
-
+            self.successful_post_listing_for_test_blog()
 
     def test_manage_update_post_not_authenticated(self):
         url = reverse("pinax_blog:manage_post_update", kwargs={"post_pk": "1"})
@@ -263,17 +278,7 @@ class TestViews(TestBlog):
     def test_manage_delete_post_author(self):
 
         with self.login(self.user):
-            url = reverse("pinax_blog:manage_post_delete", kwargs={"post_pk": "1"})
-            response = self.client.get(url)
-            self.assertEqual(response.status_code, 200)
-            self.post(url)
-            self.assertRedirects(
-                self.last_response, reverse("pinax_blog:manage_post_list")
-            )
-            url = reverse("pinax_blog:manage_post_list")
-            response = self.client.get(url)
-            self.assertEqual(response.context_data["post_list"].count(), 1)
-            self.assertIn(self.apple_post, response.context_data["post_list"])
+            self.successful_deletion_for_test_blog()
 
     def test_manage_delete_post_not_author(self):
 
@@ -288,17 +293,7 @@ class TestViews(TestBlog):
         self.user.save()
 
         with self.login(self.user):
-            url = reverse("pinax_blog:manage_post_delete", kwargs={"post_pk": "1"})
-            response = self.client.get(url)
-            self.assertEqual(response.status_code, 200)
-            self.post(url)
-            self.assertRedirects(
-                self.last_response, reverse("pinax_blog:manage_post_list")
-            )
-            url = reverse("pinax_blog:manage_post_list")
-            response = self.client.get(url)
-            self.assertEqual(response.context_data["post_list"].count(), 1)
-            self.assertIn(self.apple_post, response.context_data["post_list"])
+            self.successful_deletion_for_test_blog()
 
 
 
